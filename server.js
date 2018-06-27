585a242797637eae551d3bed2fa6c9799f68c8a4
turquoise-turtle <a3j2p1@gmail.com>
2018-06-28 08:11:09 +1000

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ var port = process.env.PORT || 8080;
 // immediate abuse (e.g. denial of service). If you want to block all origins except for some,
 // use originWhitelist instead.
 var originBlacklist = parseEnvList(process.env.CORSANYWHERE_BLACKLIST);
-var originWhitelist = parseEnvList(process.env.CORSANYWHERE_WHITELIST);
+//var originWhitelist = parseEnvList(process.env.CORSANYWHERE_WHITELIST);
+var originWhitelist = parseEnvList('https://thimbleprojects.org,http://thimbleprojects.org,https://techniko.ml,http://techniko.ml');
 function parseEnvList(env) {
   if (!env) {
     return [];
