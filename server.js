624869bfcf41b91fa3faceaf7f9ebd3469cc317f
technikodev <31405747+technikodev@users.noreply.github.com>
2018-08-18 08:18:43 +1000

    Update server.js

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ var port = process.env.PORT || 8080;
 // immediate abuse (e.g. denial of service). If you want to block all origins except for some,
 // use originWhitelist instead.
 var originBlacklist = parseEnvList(process.env.CORSANYWHERE_BLACKLIST);
-//var originWhitelist = parseEnvList(process.env.CORSANYWHERE_WHITELIST);
-var originWhitelist = parseEnvList(,);
+var originWhitelist = parseEnvList(process.env.CORSANYWHERE_WHITELIST);
+//var originWhitelist = parseEnvList(,'');
 var headersWhitelist = parseEnvList(process.env.CORSANYWHERE_HEADERS);
 function parseEnvList(env) {
   if (env == 'yes') {
